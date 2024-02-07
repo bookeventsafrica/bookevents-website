d52af4e79c046abeb5fd7934b70828398a676ab6
mr-emeka <speak2c.emeka@gmail.com>
2024-02-07 19:04:16 +0100

    website wip

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,11 +5,13 @@ import Button from "../components/button";
 import Footer from "../components/footer";
 import FavoriteSVG from '/public/svg/favorite.svg';
 import { useState } from "react";
-import { FlutterWaveButton } from "flutterwave-react-v3";
 import EventSection from "../components/sections/events";
 import LocationSVG from 'public/svg/map-pin.svg'
 import CalendarSVG from 'public/svg/calendar.svg'
 import Ticket from "../components/ticket";
+import CancelIcon from "/public/svg/cancel.svg";
+import ArrowDown from '/public/svg/menu-arrow.svg';
+
 
 
 export default function Home() {
@@ -24,38 +26,43 @@ export default function Home() {
         <>
             {/* banner */}
             <header className="bg-white text-white justify-between  flex items-center  p-2 md:py-[14px] md:px-[90px]">
-                <div className="flex md:items-center">
-                    <Image src="/logo.png" alt="bookEventsAfrica_logo" width={150} height={150} />
-                </div>
+          <div className="flex md:items-center">
+            <Image src="/logo.png" alt="bookEventsAfrica_logo" width={150} height={150} />
+          </div>
 
-                <nav className="flex gap-x-[24px] cursor-pointer flex-col md:flex-row md:items-center md:justify-end text-[16px] text-primary-800">
-                    <ul className="hidden md:flex gap-x-[24px] mb-4 md:mb-0">
-                        <li><a href="/">Discover</a></li>
-                        <li><a href="/blog">Blog</a></li>
-                        <li><a>Login</a></li>
-                    </ul>
-                    <div className="hidden md:block mt-3 md:mt-0">
-                        <Button variant="primary" className="rounded-[8px] text-[16px] px-[24px] py-[10px]">Sign Up</Button>
-                    </div>
-                    {/* <HamburgerIcon onClick={toggleMenu} /> */}
-                    <div onClick={toggleMenu} className="flex md:hidden">Icon-{ }</div>
-                </nav>
+          <nav className="flex gap-x-[24px] cursor-pointer flex-col md:flex-row md:items-center md:justify-end text-[16px] text-primary-800">
+            <ul className="hidden md:flex gap-x-[24px] mb-4 md:mb-0">
+              <li><a href="/">Discover</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a>Login</a></li>
+            </ul>
+            <div className="hidden md:block mt-3 md:mt-0">
+              <Button variant="primary" className="rounded-[8px] text-[16px] px-[24px] py-[10px]">Sign Up</Button>
+            </div>
+            {/* <HamburgerIcon onClick={toggleMenu} /> */}
+            <div onClick={toggleMenu} className="flex md:hidden cursor-pointer">
+              <ArrowDown className="text-primary-800" />
+            </div>
+          </nav>
 
-                <div className={`fixed top-0 left-0 w-full overflow-hidden  bg-white z-50 py-2 shadow ${isMenuOpen ? '' : 'hidden'}`}>
-                    <div className="flex md:items-center mb-5">
-                        <Image src="/logo.png" alt="bookEventsAfrica_logo" width={150} height={150} />
-                    </div>
-                    <ul className="px-5 flex flex-col md:justify-center md:text-center text-primary-800 gap-10">
-                        <li><a href="/">Discover</a></li>
-                        <li><a href="/blog">Blog</a></li>
-                        <li><a href="/login">Login</a></li>
-                    </ul>
+          <div className={`fixed top-0 left-0 w-full overflow-hidden  bg-white z-50 py-2 shadow ${isMenuOpen ? '' : 'hidden'}`}>
+            <div className="flex justify-between items-center mb-5 px-4">
+              <Image src="/logo.png" alt="bookEventsAfrica_logo" width={150} height={150} />
+              <CancelIcon className="text-primary-800 cursor-pointer" onClick={toggleMenu} />
 
-                    <div className="px-5 my-5">
-                        <Button variant="primary" className="rounded-[8px] text-[16px] px-[24px] py-[10px] w-full font-medium">Sign Up</Button>
-                    </div>
-                </div>
-            </header>
+            </div>
+            <ul className="px-5 flex flex-col md:justify-center md:text-center text-primary-800 gap-10">
+              <li><a href="/">Discover</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="/login">Login</a></li>
+            </ul>
+
+            <div className="px-5 my-5">
+              <Button variant="primary" className="rounded-[8px] text-[16px] px-[24px] py-[10px] w-full font-medium">Sign Up</Button>
+            </div>
+          </div>
+        </header>
+
 
             <main >
                 <section className="px-[1rem] md:px-[6.18rem] mt-[49px] cursor-pointer">

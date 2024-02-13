7645d62c04606d08ffcd9dd8a9ab80378661602c
mr-emeka <speak2c.emeka@gmail.com>
2024-02-13 14:20:56 +0100

    fix env issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   const [events, setEvents] = useState([])
   useEffect(() => {
     fetch()
-    console.log(process.env);
+    console.log(process.env.NEXT_PUBLIC_API_DEV);
   }, [])
 
   const fetch = async () => {

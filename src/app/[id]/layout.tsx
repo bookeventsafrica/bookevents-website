ddf837f127a2ce74268a061f60657a99d78d7227
mr-emeka <speak2c.emeka@gmail.com>
2024-02-13 13:31:52 +0100

    update

--- src/app/[id]/layout.tsx
+++ src/app/[id]/layout.tsx
@@ -1,7 +1,34 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const url = `${process.env.NEXT_PUBLIC_API}/event/${params.id}`
+
+  // fetch data
+  const [data] = await fetch(url).then((res) => res.json());
+
+    const event = data;
+
+  return {
+    title: event.name,
+    description: event.details,
+    openGraph: {
+      images: event.image
+    },
+  };
+}
+
+
 export default function AboutLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return <section>{children}</section>
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <section>{children}</section>
+}

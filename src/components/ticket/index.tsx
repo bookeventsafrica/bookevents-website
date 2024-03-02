31aec4ddbf45ff473631df1a09226e4c0a433e72
mr-emeka <speak2c.emeka@gmail.com>
2024-03-02 15:31:34 +0100

    update

--- src/components/ticket/index.tsx
+++ src/components/ticket/index.tsx
@@ -43,7 +43,7 @@ export default function Ticket({ selectedTicket, select, tickets }: TicketProps)
 
         {/* <h6 className="font-400 text-[14px] text-[#292D32]">Tickets</h6> */}
 
-        <div className="flex mt-2 gap-4">
+        <div className="flex mt-2 gap-4 flex-wrap md:flex-row">
             {tickets.filter(ticket => ticket.available).map(ticket => {
                 return <div className={`${selectedTicket?._id == ticket._id && 'border-primary-800 border border-1.5'} flex flex-col justify-center bg-[#F1F1F1] p-3 text-center rounded-[4px]  w-[120px]`} key={ticket._id} onClick={() => select(ticket)}>
                     <p className="font-400 text-[12px]">{ticket.name}</p>

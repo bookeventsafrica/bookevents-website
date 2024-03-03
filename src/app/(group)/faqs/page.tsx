8a6c6c3554f6b4e39d2fdee085b0bc2ec908f0e8
mr-emeka <speak2c.emeka@gmail.com>
2024-03-03 08:55:30 +0100

    fix faqs page

--- src/app/(group)/faqs/page.tsx
+++ src/app/(group)/faqs/page.tsx
@@ -12,14 +12,14 @@ export const metadata: Metadata = {
 let faqs = [{
     title: 'How do i buy ticket on bookevents.africa?',
     content: <>
-        <p className="font-normal text-[18px] text-[#707070] m-0">
+        <p className="font-normal text-[14px] md:text-[18px] text-[#707070] m-0">
             Tickets for events can be purchased via bookeventsafrica.com website. Go to the event you’d like to purchase tickets for and on that event page, both free, online and paid tickets will be clearly marked.
         </p>
     </>
 }, {
     title: 'How do I make money from bookeventsafrica?',
     content: <>
-        <p className="font-normal text-[18px] text-[#707070] m-0">
+        <p className="font-normal text-[14px] md:text-[18px] text-[#707070] m-0">
             By signing up for an organizer account/hosting events. sign up today
         </p>
     </>
@@ -29,13 +29,13 @@ function FAQ() {
     return <>
         <Navbar />
 
-        <section className="md:px-[100px] bg-[#F1F2F6] md:py-[56px]">
+        <section className="px-[1rem] md:px-[100px] bg-[#F1F2F6] md:py-[56px]">
             <h3 className="text-center text-[35px] font-bold text-[#292D32] mb-[54px]">Frequently Asked Questions</h3>
             {faqs.map((faq, index) => {
                 return <Accordion title={faq.title} content={faq.content} index={index} key={index} />
             })}
 
-            <div className="w-full md:w-[708px] mx-auto mt-[30px] md:mt-[60px]">
+            <div className="w-full md:w-[708px] mx-auto  mb-4 mt-[30px] md:mt-[60px]">
                 <h4 className="text-center font-bold text-[25px] text-[#292D32]">Still have a question?</h4>
                 <p className="font-normal text-[18px] mt-[24px] text-[#292D32] text-center">If you cannot find the answer to the question on our FAQ, you can always {" "}
                     contact at   <a href="mailto:support@bookevents.africa" className="text-primary-800 text-underline">

00ef958bc65c3138e65194ec8402e34690a0f6ac
mr-emeka <speak2c.emeka@gmail.com>
2024-05-21 16:40:37 +0100

    update blog

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -17,30 +17,28 @@ const BlogCard = ({ post }: { post: any }) => {
     <div className="mt-[24px]">
       <div>
         <h3 className="text-[24px] font-[700] text-[#292D32] mb-[12px]">{post.title}</h3>
-        <p className="my-0  text-[16px] font-[400]">{post.description && post.description.substring(0, 100) + "[...]"} </p>
+        <p className="my-0  text-[16px] font-[400]">{post.description && post.description.substring(0, 100) + "..."} </p>
       </div>
-      <div className="flex items-center mt-[12px] justify-between flex-wrap">
-        <div className="flex flex-row flex-1 items-center gap-2">
+      <div className="flex items-center mt-[12px] gap-2 flex-wrap">
+        <div className="flex flex-row items-center gap-2">
           <Image
             src={
               urlForImage(post.authorImage)
             }
             alt={post?.authorImage?.alt}
-            width={41}
-            height={41}
-            className="h-[41px] w-[41px] rounded-full"
+            width={24}
+            height={24}
+            className="h-[24px] w-[24px] rounded-full"
           />
           <p className="text-[14px] text-[#292D32]">{post.authorName}</p>
         </div>
-        <div className="flex flex-1  flex-col items-end">
           <p className="text-[14px] text-[#292D32] ">
-        {new Date(post.publishedAt).toLocaleDateString("en-US", {
-                        day: "numeric",
-                        month: "long",
-                        year: "numeric",
-                    })}
+            - {new Date(post.publishedAt).toLocaleDateString("en-US", {
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })}
           </p>
-        </div>
       </div>
     </div>
   </a>

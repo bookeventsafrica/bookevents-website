ff334a1238a66f8f2685d246dd03669539a528fe
mr-emeka <speak2c.emeka@gmail.com>
2024-07-17 11:59:27 +0100

    fix event card

--- src/components/events/index.tsx
+++ src/components/events/index.tsx
@@ -1,46 +1,61 @@
 import { formatDate, IEvent, isEventPast } from "@/utils";
 import moment from "moment";
+import Image from "next/image";
+
+export default async function Item({ event, ...props }: { event: IEvent }) {
 
 
-export default function Item({ event, ...props }: { event: IEvent }) {
-    return <div className="w-full bg-white cursor-pointer relative rounded-[16px]">
-        <a href={`/${event.slug}`}>
-            {/* <div
-                className="h-[220px] object-cover object-[center_top] w-full rounded-t-[16px]"
-                style={{
-                    background: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), url(${event.image}), lightgray 50% / cover no-repeat`
-                }}
-
-            ></div> */}
-            {/*  eslint-disable-next-line @next/next/no-img-element */}
-            <img src={event.image} alt="" className="h-[220px] w-full object-cover"  loading="lazy"/>
-        </a>
-        <div className="top-0 px-[22px] py-[19px] absolute flex justify-between w-full">
-            <div className="bg-white py-[4px] px-2 rounded-[6px] text-[12px] font-light text-[#000] shadow-sm">
-                {event.type}
-            </div>
-
-           {isEventPast(event.eventDate) && <div className="bg-white py-[4px] px-2 rounded-[6px] text-[12px] font-bold text-red-600 shadow-sm uppercase ">Expired</div>}
+  return (
+    <div className="w-full bg-white cursor-pointer  rounded-[16px] relative">
+      <a href={`/${event.slug}`}>
+        <div className="relative w-full h-[258px] ">
+          <Image
+            src={event.image}
+            alt=""
+            fill
+            style={{ objectFit: "cover" }}
+            className="rounded-t-[16px]"
+            loading="eager"
+          />
+        </div>
+      </a>
+      <div className="top-0 px-[22px] py-[19px] absolute flex justify-between w-full">
+        <div className="bg-white py-[4px] px-2 rounded-[6px] text-[12px] font-light text-[#000] shadow-sm">
+          {event.type}
         </div>
 
-        <a href={`/${event.slug}`} className="no-underline">
-            <div className="px-[20px] py-[15px] flex items-start gap-5 lg:gap-0">
-                <div className="flex flex-col flex-[.15]">
-                    <h5 className="font-[700] text-[16px] text-primary-800 m-0">{formatDate(event.eventDate).toUpperCase()}</h5>
-                    <h3 className="text-[#000] font-[700] text-[25px] m-0">{moment(event.eventDate).format('DD')}</h3>
+        {isEventPast(event.eventDate) && (
+          <div className="bg-white py-[4px] px-2 rounded-[6px] text-[12px] font-bold text-red-600 shadow-sm uppercase ">
+            Expired
+          </div>
+        )}
+      </div>
 
-                    <h6 className="!text-primary-800 text-[13px]">{moment(event.eventTime, ['HH:mm']).format('HH:mm A')}</h6>
-                </div>
+      <a href={`/${event.slug}`} className="no-underline">
+        <div className="px-[20px] py-[15px] flex items-start gap-5 lg:gap-0">
+          <div className="flex flex-col flex-[.15]">
+            <h5 className="font-[700] text-[16px] text-primary-800 m-0">
+              {formatDate(event.eventDate).toUpperCase()}
+            </h5>
+            <h3 className="text-[#000] font-[700] text-[25px] m-0">
+              {moment(event.eventDate).format("DD")}
+            </h3>
 
-                <div className="flex flex-col gap-[12px] flex-1">
-                    <span className="text-[16px] font-[700]  text-[#292D32] ">
-                        {event.name}
-                    </span>
-                    <span className="text-[14px] font-[400] text-[#707070]">
-                        {event.location}
-                    </span>
-                </div>
-            </div>
-        </a>
+            <h6 className="!text-primary-800 text-[13px]">
+              {moment(event.eventTime, ["HH:mm"]).format("HH:mm A")}
+            </h6>
+          </div>
+
+          <div className="flex flex-col gap-[12px] flex-1">
+            <span className="text-[16px] font-[700]  text-[#292D32] ">
+              {event.name}
+            </span>
+            <span className="text-[14px] font-[400] text-[#707070]">
+              {event.location}
+            </span>
+          </div>
+        </div>
+      </a>
     </div>
+  );
 }
--- src/components/events/type-events/index.tsx
+++ src/components/events/type-events/index.tsx
@@ -1,28 +1,29 @@
-import EventSection from '@/components/sections/events';
-import { IEvent } from '@/utils';
-import axios from 'axios';
-import React from 'react';
+import EventSection from "@/components/sections/events";
+import { IEvent } from "@/utils";
+import axios from "axios";
+import React from "react";
 
 // import { Container } from './styles';
 export enum TypeEventsEnum {
-    UPCOMING_EVENTS = 'Upcoming Events',
-    PAST_EVENTS = 'Past Events',
-DISCOVER_EVENTS = 'Discover Events'
+  UPCOMING_EVENTS = "Upcoming Events",
+  PAST_EVENTS = "Past Events",
+  DISCOVER_EVENTS = "Discover Events",
 }
-const fetch = async (type: TypeEventsEnum,) => {
-    try {
-        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_ENV === 'development' ? process.env.NEXT_PUBLIC_API_DEV : process.env.NEXT_PUBLIC_API_PROD}/event/type/${type}?published=1&sort=-1`)
-        return data.data
-    } catch (err) {
-    }
-}
+const fetch = async (type: TypeEventsEnum) => {
+  try {
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_ENV === "development" ? process.env.NEXT_PUBLIC_API_DEV : process.env.NEXT_PUBLIC_API_PROD}/event/type/${type}?published=1&sort=-1`
+    );
+    return data.data;
+  } catch (err) {}
+};
 const TypeEvents = async ({ type }: { type: TypeEventsEnum }) => {
-    const events: IEvent[] = await fetch(type)
-    return <section className="lg:px-[6.18rem] bg-[#F1F2F6] py-[50px] ">
-
-        <EventSection title={type} events={events} more={true} />
-
+  const events: IEvent[] = await fetch(type);
+  return (
+    <section className="lg:px-[6.18rem] bg-[#F1F2F6] py-[50px] ">
+      <EventSection title={type} events={events} more={true} />
     </section>
-}
+  );
+};
 
 export default TypeEvents;
